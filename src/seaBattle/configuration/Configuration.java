66b2175990830439ebcd00e800d3de51f9ed2f86
T1MON <derbindima5@gmail.com>
2020-11-04 15:55:24 +0300

    Only starting

---
+++ src/seaBattle/configuration/Configuration.java
@@ -0,0 +1,16 @@
+package seaBattle.configuration;
+
+import java.util.HashMap;
+
+public class Configuration { // TODO: Isn't finished
+    private final HashMap<String, Object> conf;
+
+    public Configuration(String path) {
+        this.conf = new HashMap<>();
+
+
+    }
+
+
+
+}
